f272afd25f953446151f7c081f800062c1c9d415
Santos <realsantosm@gmail.com>
2022-03-18 13:33:27 +0530

    new program

---
+++ 01_weekOne/main.js
@@ -0,0 +1,24 @@
+
+
+// let aloo = 5;
+// if (aloo === 5) {
+//     let a = 2;
+//     console.log(" Value inside : " +a);
+// }
+// console.log("Outside Value: "+aloo);
+
+
+// var aloo = 5;
+// if (aloo === 5) {
+//     var a = 2;
+
+//     console.log(" Value inside : " +a);
+// }
+// console.log("Outside Value: "+aloo);
+
+var multiply = function (x, y) {
+    return x * y;
+};
+
+const multiply = (x, y) => x * y;
+
